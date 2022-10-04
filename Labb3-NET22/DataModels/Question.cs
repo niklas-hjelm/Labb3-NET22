90de9a753122a094144ce73c7180c5d64aac3e98
Niklas <niklas.hjelm@iths.se>
2022-10-04 10:57:38 +0200

    Add project files.

---
+++ Labb3-NET22/DataModels/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace Labb3_NET22.DataModels;
+
+public class Question
+{
+    public string Statement { get; }
+    public string[] Answers { get; }
+    public int CorrectAnswer { get; }
+}
---
+++ Labb3-NET22/DataModels/Quiz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb3_NET22.DataModels;   
+
+public class Quiz
+{
+    private IEnumerable<Question> _questions;
+    private string _title = string.Empty;
+    public IEnumerable<Question> Questions => _questions;
+    public string Title => _title;
+
+    public Quiz()
+    {
+        _questions = new List<Question>();
+    }
+
+    public Question GetRandomQuestion()
+    {
+        throw new NotImplementedException("A random Question needs to be returned here!");
+    }
+
+    public void AddQuestion(string statement, int correctAnswer, params string[] answers)
+    {
+        throw new NotImplementedException("Question need to be instantiated and added to list of questions here!");
+    }
+
+    public void RemoveQuestion(int index)
+    {
+        throw new NotImplementedException("Question at requested index need to be removed here!");
+    }
+}
